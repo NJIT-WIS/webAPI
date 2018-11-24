c010a483e1efc1485d8e3b225b35f7a715bf3a0b
Max A <mla23@njit.edu>
2018-11-23 21:44:26 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -75,20 +75,6 @@ return [
             'sslmode' => 'prefer',
         ],
         
-         'testing_travis' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
-            'username'  => env('DB_TEST_USERNAME', 'homestead'),
-            'password'  => env('DB_TEST_PASSWORD', 'secret'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-
-
-        ],
-        
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
